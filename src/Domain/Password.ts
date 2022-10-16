283cb9d6ad2d07b3a04c5cc9d13cf4159ab05d4a
NathanSoa_ <53349560+NathanSoa@users.noreply.github.com>
2022-10-16 15:58:29 -0300

    doc: add jsdoc on password value object

--- src/Domain/Password.ts
+++ src/Domain/Password.ts
@@ -1,10 +1,20 @@
 import bcrypt from 'bcryptjs'
 
+/**
+* Type containing the must have for any password object
+*/
 type passwordProperties = {
     password: string,
     hashed?: boolean
 }
 
+/**
+* Class that represent the value object of any user password. It has a private constructor,
+* new instances should be create be using the static method create().
+* This class uses bcrypt to encrypt the string value.
+* @constructor
+* @param {passwordProperties} props - object that have all values for a password
+*/
 export class Password {
     private password: string
     private hashed: boolean
@@ -14,6 +24,12 @@ export class Password {
         this.password = props.password
     }
 
+    /**
+    * method that return a new instance of password if it has valid length,
+    * if invalid length password is sent, it will throw an error.
+    * @constructor
+    * @param {passwordProperties} props - object that have all values for a password
+    */
     static create({ password, hashed = false}: passwordProperties) {        
         if(Password.hasInvalidLength(password)){
             throw new Error("Password has Invalid length")
@@ -33,6 +49,10 @@ export class Password {
         return this.password === plainPassword
     }
 
+    /**
+    * method that return the password hashed value if it is not hashed yet
+    * @return {Promise<string>} - promise with password hashed value 
+    */
     public async getHashed(): Promise<string> {
         if (this.hashed) {
           return this.password
@@ -41,10 +61,16 @@ export class Password {
         return await bcrypt.hash(this.password, 8)
     }
 
+    /**
+    * method that return if a string value has not valid length to be used as password
+    */
     static hasInvalidLength(password: string): boolean {
         return !this.hasValidLength(password)
     }
 
+    /**
+    * method that return if a string value has valid length to be used as password
+    */
     static hasValidLength(password: string): boolean {
         return (password.trim().length > 8) && (password.trim().length < 255)
     }
