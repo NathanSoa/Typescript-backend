6e5af043d2fb408cfaeb06911c937aa596e15118
NathanSoa_ <53349560+NathanSoa@users.noreply.github.com>
2022-10-15 21:22:16 -0300

    add model maper to movie entity coming from database

---
+++ src/Utils/MovieMapper.ts
@@ -0,0 +1,25 @@
+import { Movie } from "../Domain/Movie";
+
+type MovieDB = {
+    id: string,
+    title: string,
+    year: string,
+    synopsis: string,
+    genres: genre[]
+}
+
+type genre = {
+    GenreName: string
+}
+
+export class MovieMapper {
+    static toDomain(rawData: MovieDB){
+        const id = rawData.id
+        const title = rawData.title
+        const year = rawData.year
+        const synopsis = rawData.synopsis
+        const genres: string[] = Array.from(rawData.genres.map(each => each.GenreName))
+
+        return new Movie({title, year, synopsis, genres}, id)
+    }
+}
