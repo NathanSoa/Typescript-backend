43c2149bb7f5cd8d9db61453297b99c748e7a25d
NathanSoa_ <53349560+NathanSoa@users.noreply.github.com>
2022-10-17 20:11:35 -0300

    refactor: removed unused property

--- src/Domain/User.ts
+++ src/Domain/User.ts
@@ -2,7 +2,6 @@ import { v4 as uuid } from 'uuid'
 import { Password } from './Password'
 
 type userProperties = {
-    name: string,
     email: string, 
     password: Password
 }
@@ -10,23 +9,17 @@ type userProperties = {
 export class User {
 
     private id: string
-    private name: string
     private email: string
     private password: Password
 
     constructor(props: userProperties, id?: string){
         this.id = id || uuid()
-        this.name = props.name
         this.email = props.email
         this.password = props.password
     }
 
     getID(){
         return this.id
-    }
-
-    getName(){
-        return this.name
     }
 
     getEmail(){
